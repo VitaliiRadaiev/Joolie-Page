7b39ed7e7be4490081579b4c156c94f1af24b1fa
Vitalii Radaiev <hf.chelenger@gamil.com>
2020-12-14 14:25:58 +0200

    added page

--- js/app.js
+++ js/app.js
@@ -630,95 +630,112 @@ function selects_update_all() {
 	}
 }
 
-// //Placeholers
-// let inputs = document.querySelectorAll('input[data-value],textarea[data-value]');
-// inputs_init(inputs);
+//Placeholers
+let inputs = document.querySelectorAll('input');
+inputs_init(inputs);
 
-// function inputs_init(inputs) {
-// 	if (inputs.length > 0) {
-// 		for (let index = 0; index < inputs.length; index++) {
-// 			const input = inputs[index];
-// 			const input_g_value = input.getAttribute('data-value');
-// 			input_placeholder_add(input);
-// 			if (input.value != '' && input.value != input_g_value) {
-// 				input_focus_add(input);
-// 			}
-// 			input.addEventListener('focus', function (e) {
-// 				if (input.value == input_g_value) {
-// 					input_focus_add(input);
-// 					input.value = '';
-// 				}
-// 				if (input.getAttribute('data-type') === "pass") {
-// 					input.setAttribute('type', 'password');
-// 				}
-// 				if (input.classList.contains('_date')) {
-// 					/*
-// 					input.classList.add('_mask');
-// 					Inputmask("99.99.9999", {
-// 						//"placeholder": '',
-// 						clearIncomplete: true,
-// 						clearMaskOnLostFocus: true,
-// 						onincomplete: function () {
-// 							input_clear_mask(input, input_g_value);
-// 						}
-// 					}).mask(input);
-// 					*/
-// 				}
-// 				if (input.classList.contains('_phone')) {
-// 					//'+7(999) 999 9999'
-// 					//'+38(999) 999 9999'
-// 					//'+375(99)999-99-99'
-// 					input.classList.add('_mask');
-// 					Inputmask("+375 (99) 9999999", {
-// 						//"placeholder": '',
-// 						clearIncomplete: true,
-// 						clearMaskOnLostFocus: true,
-// 						onincomplete: function () {
-// 							input_clear_mask(input, input_g_value);
-// 						}
-// 					}).mask(input);
-// 				}
-// 				if (input.classList.contains('_digital')) {
-// 					input.classList.add('_mask');
-// 					Inputmask("9{1,}", {
-// 						"placeholder": '',
-// 						clearIncomplete: true,
-// 						clearMaskOnLostFocus: true,
-// 						onincomplete: function () {
-// 							input_clear_mask(input, input_g_value);
-// 						}
-// 					}).mask(input);
-// 				}
-// 				form_remove_error(input);
-// 			});
-// 			input.addEventListener('blur', function (e) {
-// 				if (input.value == '') {
-// 					input.value = input_g_value;
-// 					input_focus_remove(input);
-// 					if (input.classList.contains('_mask')) {
-// 						input_clear_mask(input, input_g_value);
-// 					}
-// 					if (input.getAttribute('data-type') === "pass") {
-// 						input.setAttribute('type', 'text');
-// 					}
-// 				}
-// 			});
-// 			if (input.classList.contains('_date')) {
-// 				datepicker(input, {
-// 					customDays: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
-// 					customMonths: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
-// 					formatter: (input, date, instance) => {
-// 						const value = date.toLocaleDateString()
-// 						input.value = value
-// 					},
-// 					onSelect: function (input, instance, date) {
-// 						input_focus_add(input.el);
-// 					}
-// 				});
-// 			}
-// 		}
-// 	}
-// }
+function inputs_init(inputs) {
+	if (inputs.length > 0) {
+		for (let index = 0; index < inputs.length; index++) {
+			const input = inputs[index];
+
+			if (input.classList.contains('_phone')) {
+				//'+7(999) 999 9999'
+				//'+38(999) 999 9999'
+				//'+375(99)999-99-99'
+				let maskValue = input.dataset.mask;
+				input.classList.add('_mask');
+				Inputmask(maskValue, {
+					//"placeholder": '',
+					clearIncomplete: true,
+					clearMaskOnLostFocus: true,
+					onincomplete: function () {
+						//input_clear_mask(input, input_g_value);
+					}
+				}).mask(input);
+			}
+
+			//const input_g_value = input.getAttribute('data-value');
+			//input_placeholder_add(input);
+			// if (input.value != '' && input.value != input_g_value) {
+			// 	input_focus_add(input);
+			// }
+			// input.addEventListener('focus', function (e) {
+			// 	if (input.value == input_g_value) {
+			// 		input_focus_add(input);
+			// 		input.value = '';
+			// 	}
+			// 	if (input.getAttribute('data-type') === "pass") {
+			// 		input.setAttribute('type', 'password');
+			// 	}
+			// 	if (input.classList.contains('_date')) {
+			// 		/*
+			// 		input.classList.add('_mask');
+			// 		Inputmask("99.99.9999", {
+			// 			//"placeholder": '',
+			// 			clearIncomplete: true,
+			// 			clearMaskOnLostFocus: true,
+			// 			onincomplete: function () {
+			// 				input_clear_mask(input, input_g_value);
+			// 			}
+			// 		}).mask(input);
+			// 		*/
+			// 	}
+			// 	if (input.classList.contains('_phone')) {
+			// 		//'+7(999) 999 9999'
+			// 		//'+38(999) 999 9999'
+			// 		//'+375(99)999-99-99'
+			// 		input.classList.add('_mask');
+			// 		Inputmask("+375 (99) 9999999", {
+			// 			//"placeholder": '',
+			// 			clearIncomplete: true,
+			// 			clearMaskOnLostFocus: true,
+			// 			onincomplete: function () {
+			// 				input_clear_mask(input, input_g_value);
+			// 			}
+			// 		}).mask(input);
+			// 	}
+			// 	if (input.classList.contains('_digital')) {
+			// 		input.classList.add('_mask');
+			// 		Inputmask("9{1,}", {
+			// 			"placeholder": '',
+			// 			clearIncomplete: true,
+			// 			clearMaskOnLostFocus: true,
+			// 			onincomplete: function () {
+			// 				input_clear_mask(input, input_g_value);
+			// 			}
+			// 		}).mask(input);
+			// 	}
+			// 	form_remove_error(input);
+			// });
+			// input.addEventListener('blur', function (e) {
+			// 	if (input.value == '') {
+			// 		input.value = input_g_value;
+			// 		input_focus_remove(input);
+			// 		if (input.classList.contains('_mask')) {
+			// 			input_clear_mask(input, input_g_value);
+			// 		}
+			// 		if (input.getAttribute('data-type') === "pass") {
+			// 			input.setAttribute('type', 'text');
+			// 		}
+			// 	}
+			// });
+			// if (input.classList.contains('_date')) {
+			// 	datepicker(input, {
+			// 		customDays: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+			// 		customMonths: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+			// 		formatter: (input, date, instance) => {
+			// 			const value = date.toLocaleDateString()
+			// 			input.value = value
+			// 		},
+			// 		onSelect: function (input, instance, date) {
+			// 			input_focus_add(input.el);
+			// 		}
+			// 	});
+			// }
+		}
+	}
+}
 // function input_placeholder_add(input) {
 // 	const input_g_value = input.getAttribute('data-value');
 // 	if (input.value == '' && input_g_value != '') {
